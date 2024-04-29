152c2f4bb3c883995c1adcad64ada7c979132567
federico_viganò <fede_viga@hotmail.it>
2024-04-29 19:12:58 +0200

    aggiunte description e link github

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -6,18 +6,36 @@ export default {
     project: Object,
   },
 };
+
 </script>
 
 <template>
 
     <div class="col-6">
-        <div class="card">
-          <h2 class="text-center py-4">
+        
+        <div class="card h-100">
+
+            <h2 class="text-center py-4">
             Progetto: {{ project.name }}
-          </h2>
+            </h2>
+
+            <div class="info text-center">
+
+                <p>
+                    {{ project.description }}
+                </p>
+
+                <p>
+                    {{ project.github }}
+                </p>
+                
+            </div>
+            
         </div>
     </div>
 
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
