adfe20e62ccb7c62a454267c50592f5410a0a55c
emanuele-pogliari <manu.pogliari@gmail.com>
2024-04-29 18:36:55 +0200

    milestone 4 completata

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
-<script setup>
-import axios from 'axios';
+<script>
+import axios from "axios";
 
+export default {
+  data() {
+    return {
+      projects: [],
+
+      baseApiUrl: "http://127.0.0.1:8000/api/",
+    };
+  },
+
+  methods: {
+    apiCall() {
+      axios.get(this.baseApiUrl + "projects", {}).then((res) => {
+        console.log(res.data.results);
+        this.projects = res.data.results;
+      });
+    },
+  },
+
+  mounted() {
+    this.apiCall();
+  },
+};
 </script>
 
 <template>
   <div class="container py-5">
     <h1>Projects</h1>
+    <ul>
+      <li v-for="project in projects">
+        {{ project.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
