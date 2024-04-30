eef6a55d4ca35a447765308ad7388f505e776ed9
federico_viganò <fede_viga@hotmail.it>
2024-04-30 16:14:58 +0200

    import singleproject in router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import HomePage from './pages/HomePage.vue';
+import SingleProject from './pages/SingleProject.vue';
 
 const router = createRouter ({
 
